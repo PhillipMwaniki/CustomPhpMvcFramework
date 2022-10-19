ab178cee5e1fbf20f0bcb3dcba8312d8623c269c
Phillip Mwaniki <codezeroprivate@gmail.com>
2022-10-20 00:35:31 +0300

    Added forms

--- views/register.php
+++ views/register.php
@@ -5,14 +5,14 @@
         <div class="row">
             <div class="col">
                 <div class="mb-3">
-                    <label for="firstname" class="form-label">First Name </label>
-                    <input type="text" class="form-control" name="firstname" placeholder="Why are you emailing us?">
+                    <label for="first_name" class="form-label">First Name </label>
+                    <input type="text" class="form-control" name="first_name" placeholder="Why are you emailing us?">
                 </div>
             </div>
             <div class="col">
                 <div class="mb-3">
-                    <label for="lastname" class="form-label">Last Name</label>
-                    <input type="text" name="lastname" class="form-control" placeholder="name@example.com">
+                    <label for="last_name" class="form-label">Last Name</label>
+                    <input type="text" name="last_name" class="form-control" placeholder="name@example.com">
                 </div>
             </div>
         </div>
@@ -23,12 +23,12 @@
 
         <div class="mb-3">
             <label for="password" class="form-label">Password</label>
-            <input type="password" name="password" class="form-control" placeholder="name@example.com">
+            <input type="password" name="password" class="form-control" placeholder="password">
         </div>
 
         <div class="mb-3">
-            <label for="confirmpassword" class="form-label">Confirm Password</label>
-            <input type="password" name="confirmpassword" class="form-control" placeholder="name@example.com">
+            <label for="password_confirmation" class="form-label">Confirm Password</label>
+            <input type="password" name="password_confirmation" class="form-control" placeholder="confirm password">
         </div>
 
         <button class="btn btn-primary" type="submit">Submit</button>
